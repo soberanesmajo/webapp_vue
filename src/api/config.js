adbb9899f8b4ed8f53fcaf7a4028d40f4d7d3684
soberanesmajo <soberanesmajo@gmail.com>
2019-04-04 13:01:32 -0600

    first commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	platzimusic
+// API key	c8c19723345e44acfb6431b7ba8eb1e6
+// Shared secret	63e3e1a57f6e8eeb58a4ac4163aca9df
+// Registered to	soberanesmajo
+
+export default {
+    appname: 'platzimusic',
+    apiKey: 'c8c19723345e44acfb6431b7ba8eb1e6',
+    secret: '63e3e1a57f6e8eeb58a4ac4163aca9df',
+    registeredTo: 'soberanesmajo'
+}
